9d8aa8533ec1d20b47f5ab36e35e7b39389cddaa
vinicius-005 <vvalenga15@gmail.com>
2022-12-26 22:58:59 -0300

    commit

---
+++ js/function.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn');
+
+
+btn.addEventListener('click', ()=>{
+    alert('Voce acredita agora que n sou golpe e deseja continuar nosso compromisso?')
+
+    document.querySelector('.box').style.display = 'block';
+})
